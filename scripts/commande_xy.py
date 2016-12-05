66a9d5421a3a1c3252e4908882a8fd2538f664a4
aplyer <aurelien@plyer.fr>
2016-12-05 16:17:13 +0000

    ajout commande en lacet + waypoints

--- scripts/commande_xy.py
+++ scripts/commande_xy.py
@@ -13,8 +13,13 @@ from ardrone_autonomy.msg import Navdata # for receiving navdata feedback
 import numpy as np 
 import time
 
+threshold = 15
+
 def isCloseEnough(value, target):
-    return np.abs(target-value) < 0.10
+    return np.abs(target-value) < threshold
+
+def isAngleCloseEnough(value, target):
+    return isCloseEnough(value, target) or isCloseEnough(value+360, target) or isCloseEnough(value-360, target)
 
 # variables globales
 commande = Twist()
@@ -26,18 +31,21 @@ rospy.init_node('commande_z', anonymous=True)
 pubCommande = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
 
-WP = [0.3, 0.6, 1, 1.4] #en m
-# WP = [1.0, 1.0]
-timer = 5   #in s
+RotationWP = [0, 90, 180, 360] #en degres
+
+timer = 4   #in s
 nextWP = 0
 isWaiting = False
 startTime = time.time()
 altitudeRef = 0.8
 
+cmd_x = 0
+cmd_y = 0
+
 def GoToNextWP():
     global nextWP
     global isWaiting
-    if nextWP < len(WP)-1:
+    if nextWP < len(RotationWP)-1:
         nextWP += 1
         isWaiting = False
 
@@ -47,43 +55,51 @@ def getAltitude(data):
     global isWaiting
     global timer
     global startTime
+    global cmd_x
+    global cmd_y
     
     # lecture donnee altitude (en m)
     altitude = data.altd / 1000.0
     tag_xc = 0
-    tag_yx = 0
-    tag_width = -1
-    tag_height = -1
+    tag_yc = 0
     tag_orientation = -1
-    tag_distance = -1
-    cmd_x = 0
-    cmd_y = 0
+
     k_xy = 0.25
+    k_angle = 4
+    
+    cmd_angular_z = 0
+    
+    angle_target = RotationWP[nextWP]
+    
+    print("angle target : ", angle_target)
     
     if data.tags_count >= 1:
         tag_xc = data.tags_xc[0]
         tag_yc = data.tags_yc[0]
-        tag_width = data.tags_width[0]
-        tag_height = data.tags_height[0]
-        tag_distance = data.tags_distance[0]
         tag_orientation = data.tags_orientation[0]
         
-        cmd_x = -((tag_yc-500.0)/500.0)*k_xy
-        cmd_y = -((tag_xc-500.0)/500.0)*k_xy
+        cmd_x = -((tag_yc-500.0)/500.0)*k_xy #Forward/backward
+        cmd_y = -((tag_xc-500.0)/500.0)*k_xy #Left/right
         
-    # affichage sur la console (pour info)
-    rospy.loginfo("altitude (m) = %f \n", altitude)
-
-    # calcul de la commande
-    # *********** A MODIFIER EN TP *************
-    if isCloseEnough(altitude, altitudeRef):
-        if isWaiting:
-            if time.time() - startTime >= timer:
-                GoToNextWP()
-                isWaiting = False
-        else:
-            startTime = time.time()
-            isWaiting = True
+        # Calul de l'angle
+        cmd_angular_z = tag_orientation - angle_target
+        if cmd_angular_z > 180:
+            cmd_angular_z = 360 - cmd_angular_z
+        if cmd_angular_z > 360:
+            cmd_angular_z -= 360.0
+        elif cmd_angular_z < 0:
+            cmd_angular_z += 360.0
+        cmd_angular_z /= 360.0
+        cmd_angular_z *= k_angle
+        
+        if isAngleCloseEnough(tag_orientation, angle_target):
+            if isWaiting:
+                if time.time() - startTime >= timer:
+                    GoToNextWP()
+                    isWaiting = False
+            else:
+                startTime = time.time()
+                isWaiting = True
     
     # commande.linear.z = (altitudeRef - altitude)*1.5 #0.75
 
@@ -93,15 +109,14 @@ def getAltitude(data):
     commande.linear.y = cmd_y
     rospy.loginfo("Y correction : %f", cmd_y )
     
-    # ****************************************
-
-    # pblication de la commande
+    commande.angular.z = cmd_angular_z
+    rospy.loginfo("Angular correction : %f", cmd_angular_z)    
+    
+    # publication de la commande
     pubCommande.publish(commande)    
-
 
 # declaration d'un subscriber : appelle getAltitude a chq arrivee d'une donnee sur le topic Navdata
 rospy.Subscriber("/ardrone/navdata", Navdata, getAltitude)
-
 
 
 # fonction main executee en boucle 
